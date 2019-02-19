fcc21591562fbf41d4bd8f013e9302df632124c5
loewenkc <loewenkc@mit.edu>
2019-02-18 21:33:04 -0500

    adding all from 1

---
+++ simple_publisher.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+from random import randint
+
+
+#generate a random number
+
+def numgen():
+    rospy.init_node('simple_publisher')
+    pub = rospy.Publisher('my_random_float',Float32, queue_size=10)
+    rate = rospy.Rate(20) #20 Hz
+    while not rospy.is_shutdown():
+        rnd_gen=randint(0,10)
+	rospy.loginfo(rnd_gen)
+        pub.publish(rnd_gen)
+	rate.sleep()
+
+if __name__=='__main__':
+	try:
+		numgen()
+	except rospy.ROSInterruptException:
+		pass
